376eb00dc8ca6d2cf89954b8f646b45e9d2409c1
Raphael <raphael.pb.rialland@gmail.com>
2025-07-29 12:02:07 +0200

    nothing is working but commit anyway

--- rans.py
+++ rans.py
@@ -7,6 +7,8 @@ class RangeANSCoder:
             self,
             f: dict[Any, int],
             k: Optional[int] = 12,
+            l: int = 16,
+            flush_size: int = 8,
     ):
         """
         Initialize the coder, notably with alphabet frequency.
@@ -27,13 +29,21 @@ class RangeANSCoder:
                 messages, where decoding time is less important and increasing
                 the value of M would decrease efficiency too greatly.
                 Default is 12. None does not normalize F.
+            l: int
+            flush_size: int
+                Number of bits to flush when the encoded state becomes superior 
+                to H.B
 
         """
         self.f = dict(sorted(f.items(), key=lambda x: -x[1])) # highest frequency symbols first
         self.m = np.sum(list(f.values()))
-
+        
+        self.l = 2**l
+        self.flush_size = flush_size
+        self.h = self.l * 2**(self.flush_size) # H = L * 2^bits, upper limit is lower limit and however many bits we flush
+        
         if k is not None:
-            self._normalize(k)
+            self._normalize_f(k)
 
         self.c = self._calculate_c()
         self.reverse_c = self._reverse_c()
@@ -41,18 +51,16 @@ class RangeANSCoder:
     def _get_highest_k(self, k):
         for candidate_k in range(k):
             if 2**candidate_k > self.m:
-                print(candidate_k)
                 return candidate_k
         return k
 
-    def _normalize(self, k):
+    def _normalize_f(self, k):
         """
         We normalize F so that sum(F) = M = 2^k.
         """
         k = self._get_highest_k(k)
         new_m = 2**k
-        # new F_s = round_int((F_s / M) * 2^k)
-        self.f = {k: np.rint((v / self.m)*(new_m)) for k, v in self.f.items()}
+        self.f = {k: round((v / self.m)*(new_m)) for k, v in self.f.items()}
         self.m = new_m
 
 
@@ -104,14 +112,14 @@ class RangeANSCoder:
             state = self._encode_step(state, symbol)
         return state
 
-    def _encode_step(self, previous_state: int, symbol: Any) -> int:
+    def _encode_step(self, state: int, symbol: Any) -> int:
         F_s = self.f[symbol]
         C_s = self.c[symbol]
 
-        d = np.floor(previous_state / F_s)
-        r = np.mod(previous_state, F_s)
+        d = state // F_s
+        r = state % F_s
         state = d * self.m + C_s + r
-
+        
         return state
 
     def decode(self, state: int, n_symbols: int) -> list:
@@ -126,28 +134,115 @@ class RangeANSCoder:
     
     def _decode_step(self, state: int) -> tuple[Any, int]:
         # first we decode the symbol
-        r = np.mod(state, self.m)
+        r = state % self.m
         symbol = self._c_inv(r)
         
         # then we find the next state
         F_s = self.f[symbol]
         C_s = self.c[symbol]
-        d = np.floor(state / self.m)
+        d = state // self.m
         next_state = d * F_s + r - C_s
 
         return symbol, next_state
 
+    def stream_encode(
+            self,
+            sequence: list,
+        ) -> tuple[int, list]:
+        """
+        Encode a sequence of symbols into a byte array that can be written to a
+        file. Since this a stream encoding, we can accept an input sequence of
+        arbitrary length.
+        """
+        
+        stream = []
+        state = self.l # we initiate to L to make sure we are always within L < X_t < H
+        print(f"H =  {self.h}")
+        print(f"Encoding iteration 0: State = {state}")
+
+        for symbol in sequence:
+            state = self._encode_step(state, symbol)
+
+            # check if in interval
+            while state >= self.h:
+                print(f"State is too large. Converting state: {state} into ", end="")
+                print(f"TEST {((1 << self.flush_size) - 1)}")
+                print(state, ((1 << self.flush_size) - 1), state & ((1 << self.flush_size) - 1))
+                bits_to_write = state & ((1 << self.flush_size) - 1) # mask state by number of bits to flush
+                stream.append(bits_to_write)
+                state = state >> self.flush_size # right shift state by flush size
+                print(f"state: {state} and writing {bits_to_write}")
+            
+            print(f"Encoding iteration {symbol}: State = {state} Bitstream = {stream}")
+
+        state = int(state)
+        return state, stream # return final state and bitstream
+    
+    def stream_encode_to_file(
+            self,
+            sequence: list,
+            file_path,
+    ) -> None:
+        return
+        with open(file_path, 'wb'):
+            pass
+
+
+    def stream_decode(
+            self,
+            state: int,
+            bitstream: bytearray,
+            n_symbols: int,
+    ) -> list:
+        print("================")
+        print("START OF DECODING")
+        print(f"{state=}\n{bitstream=}\n{n_symbols=}")
+        print("================")
+
+        stream_idx = len(bitstream) - 1
+        sequence = []
+
+        for i in range(n_symbols):
+            # check that state isn't inferior to L and still have bits left
+            while state < self.l:
+                print("Reading from bitstream, old state: {state}", end=" ")
+                state = (state << self.flush_size) | bitstream[stream_idx]
+                print(f"New state: {state}, read bits: {bitstream[stream_idx]}")
+                stream_idx -= 1
+
+            symbol, state = self._decode_step(state)
+            print(f"Iteration: {i} Decoded symbol: {symbol} New state: {state}")
+            sequence.append(symbol)
+
+        # sequence is decoded in reverse 
+        reverse_sequence = sequence[::-1]
+        return reverse_sequence
 
 if __name__ == '__main__':
+
     f = {'A': 2, 'B': 1, 'C': 3}
-    message = ["A", "C", "B", "C", "C", "A"]
-
-    rans = RangeANSCoder(f, k=None)
-
+    message = ["A", "C", "B", "C", "C", "A"] * 1
     print(f"Message: {message}")
 
-    compressed_state = rans.encode(message)
-    print(f"Compressed state: {compressed_state}")
+    rans = RangeANSCoder(f)
 
-    decompressed_message = rans.decode(compressed_state, len(message))
-    print(f"Decoded message: {decompressed_message}")
+    # --------------------------------
+    # ----- NORMAL ENCODE/DECODE -----
+    # --------------------------------
+
+    # compressed_state = rans.encode(message)
+    # print(f"Compressed state: {compressed_state}")
+
+    # decompressed_message = rans.decode(compressed_state, len(message))
+    # print(f"Decoded message: {decompressed_message}")
+
+    # --------------------------------
+    # ----- STREAM ENCODE/DECODE -----
+    # --------------------------------
+
+    final_state, compressed_stream = rans.stream_encode(message)
+    print(f"Final state: {final_state}\nCompressed bitstream: {compressed_stream}")
+
+    decompressed_message = rans.stream_decode(final_state, compressed_stream, len(message))
+    print(f"Decompressed message from stream: {decompressed_message}")
+    print(f"Equality check: {message == decompressed_message}")
