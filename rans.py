7cd455c69ff8c4ddabfb7b99adb950e994105e3e
Raphael <raphael.pb.rialland@gmail.com>
2025-07-31 12:24:16 +0200

    changed bit writing logic and reimplemented all methods

--- rans.py
+++ rans.py
@@ -2,6 +2,8 @@ from typing import Any, Optional
 
 import numpy as np
 
+Bitstream = list[int]
+
 class RangeANSCoder:
     def __init__(
             self,
@@ -39,9 +41,8 @@ class RangeANSCoder:
                 to H.
 
         """
-        self.range_factor = 1 << range_factor
-
         self.f = dict(sorted(f.items(), key=lambda x: -x[1])) # highest frequency symbols first
+        self.a = list(self.f.keys())
         self.m = sum(list(f.values()))
 
         if k is not None:
@@ -53,8 +54,17 @@ class RangeANSCoder:
         self.flush_size = flush_size
         self.flush_mask = (1 << self.flush_size) - 1
 
+        self.range_factor = 1 << range_factor
         self.l: int = self.range_factor * self.m
-        self.h: int = self.l * self.flush_mask
+        self.h: int = self.l * (1 << self.flush_size)
+
+        self.max_shrunk_state = {}
+        for s in self.a:
+            freq = self.f[s]
+            self.max_shrunk_state[s] = self.range_factor * freq * (1 << self.flush_size) - 1
+
+        self.NUM_STATE_BITS = self.h.bit_length()
+        self.DATA_BLOCK_SIZE_BITS = 32
 
     def _get_highest_k(self, k):
         for candidate_k in range(k):
@@ -112,16 +122,7 @@ class RangeANSCoder:
         for key in self.reverse_c.keys():
             if self.reverse_c[key] <= r:
                 return key
-
-    def encode(self, sequence: list) -> int:
-        """
-        Encode a sequence of symbols into a single integer state.
-        """
-        state = 0
-        for symbol in sequence:
-            state = self._encode_symbol(state, symbol)
-        return state
-
+            
     def _encode_symbol(self, state: int, symbol: Any) -> int:
         F_s = self.f[symbol]
         C_s = self.c[symbol]
@@ -131,16 +132,6 @@ class RangeANSCoder:
         state = d * self.m + C_s + r
         
         return state
-
-    def decode(self, state: int, n_symbols: int) -> list:
-        sequence = []
-        for _ in range(n_symbols):
-            symbol, state = self._decode_symbol(state)
-            sequence.append(symbol)
-
-        # sequence is decoded in reverse 
-        reverse_sequence = sequence[::-1]
-        return reverse_sequence
     
     def _decode_symbol(self, state: int) -> tuple[Any, int]:
         # first we decode the symbol
@@ -154,95 +145,115 @@ class RangeANSCoder:
         next_state = d * F_s + r - C_s
 
         return symbol, next_state
+    
+    def _shrink_state(
+            self,
+            state: int,
+            next_symbol: Any,
+    ) -> tuple[int, Bitstream]:
+        """Shrink state by streaming out lower bits."""
+        out_bits = []
+
+        while state > self.max_shrunk_state[next_symbol]:
+            out_bits.insert(0, state & ((1 << self.flush_size) - 1))
+            state >>= self.flush_size
+
+        return state, out_bits
+
+    def _expand_state(
+            self,
+            state: int,
+            bitstream: Bitstream,
+            bits_consumed: int
+    ) -> tuple[int, int]:
+        """Expand state by reading bits from the stream."""
+        num_bits_read = 0
+        while state < self.l:
+            bits_to_read = bitstream[bits_consumed + num_bits_read: bits_consumed + num_bits_read + self.flush_size]
+            val = 0
+            for i, bit in enumerate(bits_to_read):
+                val |= bit << (len(bits_to_read) - 1 - i)
+
+            state = (state << self.flush_size) | val
+            num_bits_read += self.flush_size
+        return state, num_bits_read
+
+    def encode(self, sequence: list) -> int:
+        """
+        Encode a sequence of symbols into a single integer state.
+        """
+        state = 0
+        for symbol in sequence:
+            state = self._encode_symbol(state, symbol)
+        return state
+
+    
+    def decode(self, state: int, n_symbols: int) -> list:
+        sequence = []
+        for _ in range(n_symbols):
+            symbol, state = self._decode_symbol(state)
+            sequence.append(symbol)
+
+        # sequence is decoded in reverse 
+        reverse_sequence = sequence[::-1]
+        return reverse_sequence
 
     def stream_encode(
             self,
             sequence: list,
-        ) -> list:
+        ) -> Bitstream:
         """
         Encode a sequence of symbols into a byte array that can be written to a
         file. Since this a stream encoding, we can accept an input sequence of
         arbitrary length.
-        We encode in reverse order (LIFO).
         """ 
-        stream = []
-        # we initialize to L to make sure we are always within [L, H)
-        state: int = self.l
+        bitstream = []
+        state = self.l
 
-        # iterate in reverse order
-        for symbol in reversed(sequence):
+        for symbol in sequence:
+            state, out_bits = self._shrink_state(state, symbol)
+            bitstream = out_bits + bitstream
+
             state = self._encode_symbol(state, symbol)
-            while state >= self.h:
-                bits_to_write = state & self.flush_mask # bitmask last flush_size bits of state
-                stream.append(bits_to_write)
-                state >>= self.flush_size
-            print(f"Encoding iteration {symbol}: State = {state} Stream length = {len(stream)}")
         
-        # After encoding all symbols, flush the final state into the stream.
-        # The decoder will reconstruct this state to begin decoding.
-        print(f"Flushing final state: {state}")
-        while state > 0:
-            bits_to_write = state & self.flush_mask
-            stream.append(bits_to_write)
-            state >>= self.flush_size
-        
-        return stream # return final state and bitstream
-    
-    def stream_encode_to_file(
-            self,
-            sequence: list,
-            file_path,
-    ) -> None:
-        return
-        with open(file_path, 'wb'):
-            pass
+        final_state_bits = list(map(int, bin(state)[2:].zfill(self.NUM_STATE_BITS)))
+        bitstream = final_state_bits + bitstream
 
+        size_bits = list(map(int, bin(len(sequence))[2:].zfill(self.DATA_BLOCK_SIZE_BITS)))
+        bitstream = size_bits + bitstream
+
+        return bitstream
+        
 
     def stream_decode(
             self,
             bitstream: list,
-            n_symbols: int,
     ) -> list:
-        print("================")
-        print("START OF DECODING")
-        print(f"{bitstream=}\n{n_symbols=}")
-        print("================")
+        """Decode a stream of data."""
+        size_bits = bitstream[:self.DATA_BLOCK_SIZE_BITS]
+        num_symbols = int("".join(map(str, size_bits)), 2)
+        bits_consumed = self.DATA_BLOCK_SIZE_BITS
 
-        # 1. Reconstruct the initial state from the stream.
-        # We must read from the stream until the state is in the valid
-        # working range [L, H). This reverses the final flush of the encoder.
-        state = 0
-        while state < self.l:
-            # This should not fail unless the stream is corrupt/empty
-            if not bitstream:
-                raise ValueError("Bitstream is too short to reconstruct initial state.")
-            bits = bitstream.pop()
-            state = (state << self.flush_size) | bits
-        print(f"Reconstructed initial state: {state}")
+        state_bits = bitstream[bits_consumed : bits_consumed + self.NUM_STATE_BITS]
+        state = int("".join(map(str, state_bits)), 2)
+        bits_consumed += self.NUM_STATE_BITS
 
-        sequence = []
-
-        for i in range(n_symbols):
+        decoded_sequence = []
+        for _ in range(num_symbols):
             symbol, state = self._decode_symbol(state)
-            sequence.append(symbol)
-            print(f"Iteration: {i} Decoded symbol: {symbol} New state: {state}")
 
-            # check that state isn't inferior to L and still have bits left
-            while state < self.l:
-                if not bitstream:
-                    if i == n_symbols - 1 and state == self.l:
-                        break
-                    # only happens if stream is corrupted or final state is wrong
-                    raise ValueError("Bitstream exhausted prematurely. Check for corruption or incorrect state.")
-                bits = bitstream.pop()
-                state = (state << self.flush_size) | bits
+            state, num_bits_read = self._expand_state(state, bitstream, bits_consumed)
+            bits_consumed += num_bits_read
 
-        return sequence
+            decoded_sequence.insert(0, symbol)
+
+        assert state == self.l, f"Final state {state} does not match initial state {self.l}."
+        return decoded_sequence
 
 if __name__ == '__main__':
 
     f = {'A': 2, 'B': 1, 'C': 3}
-    message = ["A", "C", "B", "C", "C", "A"] * 1
+    message = ["A", "C", "B", "C", "C", "A"] * 4
     print(f"Message: {message}")
 
     rans = RangeANSCoder(f)
@@ -264,6 +275,6 @@ if __name__ == '__main__':
     compressed_stream = rans.stream_encode(message)
     print(f"Compressed bitstream: {compressed_stream}")
 
-    decompressed_message = rans.stream_decode(compressed_stream, len(message))
+    decompressed_message = rans.stream_decode(compressed_stream)
     print(f"Decompressed message from stream: {decompressed_message}")
     print(f"Equality check: {message == decompressed_message}")
